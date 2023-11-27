417a2fa9c93d6c152cfbde8bece572d146910395
ykwizera <144004543+ykwizera@users.noreply.github.com>
2023-11-27 14:04:42 +0000

    page1

--- tourism.py
+++ tourism.py
@@ -4,4 +4,7 @@ option = int(
     input(
         'Where would like to visit \n 1.Game Parks \n 2.Museums \n 3.Hotels \n'
     ))
+if (option > 3 or option < 1 or type(option) != int):
+    print('Choose a number from 1 to 3')
+    continue
 
