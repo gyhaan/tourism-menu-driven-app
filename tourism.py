9b66d6bafa07b3fe58af2f2b6d05ab1fb441a3f9
ykwizera <144004543+ykwizera@users.noreply.github.com>
2023-11-27 14:00:53 +0000

    tourism

--- tourism.py
+++ tourism.py
@@ -4,7 +4,4 @@ option = int(
     input(
         'Where would like to visit \n 1.Game Parks \n 2.Museums \n 3.Hotels \n'
     ))
-if (option > 3 or option < 1 or type(option) != int):
-  raise ValueError('Please Choose a number from 1 to 3')
-elif type(option) != int:
-  raise TypeError('Use numbers only')
+
