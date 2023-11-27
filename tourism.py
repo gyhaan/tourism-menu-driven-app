d912743947ef127d646cc5f3fb63500a6182f998
ykwizera <144004543+ykwizera@users.noreply.github.com>
2023-11-27 14:43:39 +0000

    ok

--- tourism.py
+++ tourism.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 print('Welcome to Rwanda, the country of thousand hills. \n')
-option = int(
-    input(
-        'Where would like to visit \n 1.Game Parks \n 2.Museums \n 3.Hotels \n'
-    ))
-if (option > 3 or option < 1 or type(option) != int):
-    print('Choose a number from 1 to 3')
-    continue
+while True:
+    option = int(
+        input(
+            'Where would like to visit \n 1.Game Parks \n 2.Museums \n 3.Hotels \n'
+        ))
+    if (option > 3 or option < 1 or type(option) != int):
+        print('Choose a number from 1 to 3')
+        continue
 
