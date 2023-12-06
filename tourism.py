863c7fa711be41777dde87b9beca102dca6ef3e4
ykwizera <144004543+ykwizera@users.noreply.github.com>
2023-12-06 13:43:45 +0000

    location

--- tourism.py
+++ tourism.py
@@ -11,11 +11,9 @@ while True:
 
   elif option == 1:
     print('We have a lot of National parks in Rwanda and each has its own variety of animals \n\n')
-    print('1. Akagera National Park located in Eastern Province of Rwanda, It is home to many animals such as: lions, elephants, buffaloes, leopards, and rhinos \n')
-
-    print('2.Nyungwe National Park located in Southern Province of Rwanda, It is home to many species of birds and ton of chimps.\n')
-
-    print('3.Volcanoes National Park found in Northern Province is home to the mountain gorillas, which are one of the most endangered animals in the world,It has several trails leading to the top of Mount Karisimbi, the highest volcano in Rwanda ')
+    print('1. Akagera National Park located in Eastern Province of Rwanda, It is home to many animals such as: lions, elephants, buffaloes, leopards, and rhinos[1.8796° S, 30.7040° E] \n')
+    print('2.Nyungwe National Park located in Southern Province of Rwanda, It is home to many species of birds and ton of chimps.[2.5282° S, 29.2784° E]\n')
+    print('3.Volcanoes National Park found in Northern Province is home to the mountain gorillas, which are one of the most endangered animals in the world,It has several trails leading to the top of Mount Karisimbi, the highest volcano in Rwanda [01°28′03″S 29°29′33″E] \n')
     break
   elif option == 2:
     print('we have many museums in Rwanda, each holding many cherishable and unique History of Our Country \n \n')
